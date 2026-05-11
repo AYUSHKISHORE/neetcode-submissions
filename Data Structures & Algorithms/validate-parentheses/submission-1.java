4352b96b9be4553424211de5a97ca518931f3350
Ayush Kishore <66210437+AYUSHKISHORE@users.noreply.github.com>
2026-05-11 16:55:57 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    /*
+        Time Complexity = O(N)
+        Space Complexity = O(N)
+
+        Approach
+            -> Use stack to identify the valid paranthesis case
+            -> Push in stack in case of opening braces
+            -> Pop in case of closing braces and compare
+
+    */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
